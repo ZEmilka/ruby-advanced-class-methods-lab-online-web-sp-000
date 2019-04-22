1348b5eb425187edbdf975ab4fe9e14daec17375
Emilia Ziemichód <emifrompoland@gmail.com>
2019-04-22 21:52:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,8 @@ class Song
     self.all.detect {|n| n.name == name}
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
 
